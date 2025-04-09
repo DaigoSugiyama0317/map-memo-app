19513343ddf4b99ce2f5a593f893617f1a638d04
DaigoSugiyama0317 <Daigosgym8323@gmail.com>
2025-04-10 01:34:17 +0900

    実装: ログイン・ログアウト

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -2,7 +2,8 @@ package controller
 
 import (
 	"net/http"
-
+	"time"
+	"os"
 	"github.com/DaigoSugiyama0317/map-memo-app/model"
 	"github.com/DaigoSugiyama0317/map-memo-app/usecase"
 	"github.com/labstack/echo/v4"
@@ -37,9 +38,41 @@ func (uc *userController) SignUp(c echo.Context) error {
 }
 
 func (uc *userController) LogIn(c echo.Context) error {
-	return nil
+	//リクエストをUser構造体に入れる。
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	//ログイン処理, tokenを設定
+	token, err := uc.uu.LogIn(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	//cookieを作成・設定
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = token
+	cookie.Expires = time.Now().Add(24*time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
 }
 
 func (uc *userController) LogOut(c echo.Context) error {
-	return nil
+	//cookieをリセット
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = ""
+	cookie.Expires = time.Now()
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
 }
