9c425defbfa90ad7c659c342cb074f979b520567
DaigoSugiyama0317 <Daigosgym8323@gmail.com>
2025-04-13 22:37:09 +0900

    実装 : Tag Repository

--- model/tag.go
+++ model/tag.go
@@ -15,7 +15,7 @@ type Tag struct {
 	UserID uint   `json:"user_id"`
 	Name   string `gorm:"size:255;not null" json:"name"`
 	Color  string `gorm:"size:50" json:"color"`
-	Index  uint   `json:"index"`
+	// Index  uint   `json:"index"`
 
 	Locations []Location `gorm:"many2many:location_tags;constraint:OnDelete:CASCADE;" json:"locations,omitempty"`
 }
--- repository/tag_repository.go
+++ repository/tag_repository.go
@@ -1,33 +1,61 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/DaigoSugiyama0317/map-memo-app/model"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type ITagRepository interface {
 	GetAllTags(tags *[]model.Tag, userId uint) error
 	CreateTag(tag *model.Tag) error
 	UpdateTag(tag *model.Tag, userId uint, tagId uint) error
-	DeleteTag(tag *model.Tag, tagId uint) error
+	DeleteTag(userId uint, tagId uint) error
 }
 
 type tagRepository struct {
 	db *gorm.DB
 }
 
-func (tr tagRepository) GetAllTags(tags *[]model.Tag, userId uint) error {
+func NewTagRepository(db *gorm.DB) ITagRepository {
+	return &tagRepository{db}
+}
 
+func (tr tagRepository) GetAllTags(tags *[]model.Tag, userId uint) error {
+	//user idを元にタグを全て取得
+	if err := tr.db.Joins("User").Where("user_id=?", userId).Order("created_at").Find(tags).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (tr tagRepository) CreateTag(tag *model.Tag) error {
-
+	if err := tr.db.Create(tag).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (tr tagRepository) UpdateTag(tag *model.Tag, userId uint, tagId uint) error {
-
+	result := tr.db.Clauses(clause.Returning{}).Where("id=? AND user_id=?", tagId, userId).Updates(model.Tag{Name: tag.Name, Color: tag.Color})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected > 1 {
+		return fmt.Errorf("object does not exist")
+	}
+	return nil
 }
 
-func (tr tagRepository) DeleteTag(tag *model.Tag, userId uint, tagId uint) error {
-	
+func (tr tagRepository) DeleteTag(userId uint, tagId uint) error {
+	result := tr.db.Where("id=? AND user_id=?", tagId, userId).Delete(&model.Tag{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected < 1 {
+		return fmt.Errorf("object does not exist")
+	}
+	return nil
 }
